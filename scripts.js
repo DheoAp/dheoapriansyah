4186bab30bcce4e0dd7cd8a1d16ba169adfaecc0
DheoAp <dheoap21@gmail.com>
2022-03-13 20:02:16 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const tombol = document.querySelector(".container-tombol");
+const layarKalkulator = document.querySelector("#layar-kalkulator");
+tombol.addEventListener("click", function (e) {
+  let tombolClick = e.target;
+  let nilaiTombolClick = tombolClick.innerText;
+
+  if (nilaiTombolClick == "C") {
+    layarKalkulator.value = "";
+  } else if (nilaiTombolClick == "<") {
+    layarKalkulator.value = layarKalkulator.value.slice(0, -1);
+  } else if (nilaiTombolClick == "=") {
+    layarKalkulator.value = eval(layarKalkulator.value);
+  } else if (nilaiTombolClick == "=") {
+    layarKalkulator.value -= layarKalkulator.value;
+  } else if (nilaiTombolClick == "=") {
+    layarKalkulator.value /= layarKalkulator.value;
+  } else if (nilaiTombolClick == "=") {
+    layarKalkulator.value *= layarKalkulator.value;
+  } else {
+    layarKalkulator.value = layarKalkulator.value + nilaiTombolClick;
+  }
+});
+
+// ========== dark mode ========== 
+const mode = document.getElementById("tombolMode");
+const dark = document.querySelectorAll('.card');
+const judulLight = document.querySelectorAll('.judul');
+
+// dark mode untuk semua card, body, text galeri 
+mode.addEventListener("click", function () {
+  document.body.classList.toggle("dakrModeBody");
+  
+  judulLight.forEach(function(jl){
+    jl.classList.toggle('text-light');
+  });
+
+  dark.forEach(function(i){
+    // dark untuk card
+    i.classList.toggle('darkMode');
+  });
+  // for(let i = 0; i < dark.length; i++){
+  //   dark[i].classList.toggle("darkMode");   
+  // }
+});
+
+
